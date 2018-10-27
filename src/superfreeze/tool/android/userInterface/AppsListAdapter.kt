65afd5444d6ebefed837b3462cd3e0d87e174f8a
Hocceruser <hocceruser-github@gmx.de>
2018-10-27 11:04:28 +0200

    Add license notes

--- src/superfreeze/tool/android/userInterface/AppsListAdapter.kt
+++ src/superfreeze/tool/android/userInterface/AppsListAdapter.kt
@@ -18,7 +18,6 @@ You should have received a copy of the GNU General Public License
 along with SuperFreezZ.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 package superfreeze.tool.android.userInterface
 
 import android.content.Context
